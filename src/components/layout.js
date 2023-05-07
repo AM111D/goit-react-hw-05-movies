5028d8aa91b8316b5b32d11a412d1b13981a0007
AM111D <Dimasssnike@icloud.com>
2023-05-07 10:22:14 -0700

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,16 @@
 import { NavLink, Outlet } from 'react-router-dom';
-import { Suspense, activeClassName } from 'react';
+import { Suspense } from 'react';
 // import { styled } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import css from './Layout.module.css';
 
-const StyledNavLink = styled(NavLink)`
-  &.${activeClassName} {
-    color: red;
-    font-weight: bold;
-  }
-`;
+// const StyledNavLink = styled(NavLink)`
+//   &.${activeClassName} {
+//     color: red;
+//     font-weight: bold;
+//   }
+// `;
 
 const Layout = () => {
   return (
@@ -19,20 +19,12 @@ const Layout = () => {
         <nav>
           <ul className={css.headerList}>
             <li className={css.headerListItem}>
-              <NavLink
-                to="/"
-                className={css.headerListItemLink}
-                activeClassName={StyledNavLink}
-              >
+              <NavLink to="/" className={css.headerListItemLink}>
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/movies"
-                className={css.headerListItemLink}
-                activeClassName={StyledNavLink}
-              >
+              <NavLink to="/movies" className={css.headerListItemLink}>
                 Movies
               </NavLink>
             </li>
