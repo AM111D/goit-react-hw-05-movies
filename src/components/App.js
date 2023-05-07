b4d09a73fc313537cd36be908ba7d19529b84734
AM111D <Dimasssnike@icloud.com>
2023-05-07 10:15:25 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ import { lazy } from 'react';
 const Home = lazy(() => import('./Pages/Home/Home'));
 const Movies = lazy(() => import('./Pages/Movies/Movies'));
 const MovieDetails = lazy(() => import('./Pages/MovieDetails/MovieDetails'));
-const Layout = lazy(() => import('./Layout'));
+const Layout = lazy(() => import('./layout'));
 
 const App = () => {
   return (
